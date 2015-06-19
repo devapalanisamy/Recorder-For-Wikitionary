f9a10a817e744e9b8ec24031db0489c98d639d1c
Deva <dev.palanisamy@gmail.com>
2015-06-19 21:12:00 +0100

    IWebService created

---
+++ WikiRecorder/WikiRecorder.Core/Source/IWebService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WikiRecorder.Core
+{
+	public interface IWebService
+	{
+		Task<string> Login(string userName, string password);
+		Task<string> Register(string userName, string emailId, string password, string confirmPassword);
+		Task<string> Upload(string baseUrl, string filePath);
+		Task<string> GetToken(string userName, string password);
+		Task<string> EditPage(string wikiContentUrl, string contentString);
+	}
+}
+
