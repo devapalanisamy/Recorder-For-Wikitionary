b2089fa27ec70de229efbf76a412d0d272df1683
Deva <dev.palanisamy@gmail.com>
2015-06-21 21:35:45 +0100

    -TinyIOC added -BaseViewModel added -FakeWebservice added

---
+++ WikiRecorder/WikiRecorder.Core/Source/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WikiRecorder.Core
+{
+	public class BaseViewModel
+	{
+		protected IWebService webservice = TinyIoC.TinyIoCContainer.Current.Resolve<IWebService>();
+
+		public BaseViewModel ()
+		{
+			
+		}
+	}
+}
+
---
+++ WikiRecorder/WikiRecorder.Core/Source/Webservice/FakeWebservice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WikiRecorder.Core
+{
+	public class FakeWebservice:IWebService
+	{
+
+		public int SleepDuration { get; set; }
+		public FakeWebservice ()
+		{
+			SleepDuration = 1;
+		}
+
+		private Task Sleep()
+		{
+			return Task.Delay(SleepDuration);
+		}
+
+			
+		public async Task<string> Login(string userName, string password)
+		{
+			await Sleep();
+			return "sessionId";
+		}
+
+		public  async Task<string> Register(string userName, string emailId, string password, string confirmPassword)
+		{
+			await Sleep();
+			return "userId";
+		}
+
+		public  async Task<string> Upload(string baseUrl, string filePath)
+		{
+			await Sleep();
+			return "URL";
+		}
+
+		public  async Task<string> GetToken(string userName, string password)
+		{
+			await Sleep();
+			return "token";
+		}
+
+		public  async Task<string> EditPage(string wikiContentUrl, string contentString)
+		{
+			await Sleep();
+			return "editStatus";
+		}
+
+	}
+}
+
